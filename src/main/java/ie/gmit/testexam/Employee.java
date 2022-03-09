581f62f50050b6743fc3858ad68c35dd2f7d781f
demps123 <G00373582@gmit.ie>
2022-03-09 10:36:11 +0000

    Tests added to check valid and invalid name

--- src/main/java/ie/gmit/testexam/Employee.java
+++ src/main/java/ie/gmit/testexam/Employee.java
@@ -10,10 +10,10 @@ public class Employee {
 
     public Employee(String title, String name, String pps, String type, int age) {
         setTitle(title);
-        this.name = name;
-        this.pps = pps;
-        this.type = type;
-        this.age = age;
+        setName(name);
+        setPps(pps);
+        setType(type);
+        setAge(age);
     }
 
     public String getTitle() {
@@ -28,4 +28,42 @@ public class Employee {
             throw new IllegalArgumentException("Invalid Title");
         }
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if(name.length() > 3) {
+            this.name = name;
+        }
+        else {
+            throw new IllegalArgumentException("Name provided is not valid");
+        }
+    }
+
+    public String getPps() {
+        return pps;
+    }
+
+    public void setPps(String pps) {
+        this.pps = pps;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
 }
